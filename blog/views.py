adf6edda435024612fa15d96690942ad5104ebdb
xRockBoyx <kp3344567@gmail.com>
2019-03-03 18:54:36 +0800

    Implement blog

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,redirect
+from django.utils import timezone
+
+from .models import Post
+# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_create(request):
+    if request.method == "POST":
+        new_title = request.POST.get('title')
+        new_text = request.POST.get('text')
+        new_post = Post(title=new_title, text=new_text)
+        
+        new_post.save()
+        
+        new_post.publish()
+        return redirect('post_list')
+    
+    return render(request, 'blog/post_create.html', {})
